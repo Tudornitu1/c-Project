8f9574a857371b0afa6d16072244d2668e16dd32
Tudornitu1 <tudornitu@yahoo.com>
2023-11-16 00:50:43 +0200

    classes

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,6 +1,58 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cstdlib>
 
-int main()
-{
-	std::cout << std::endl << "Hello";
-}
+class EventLocation {
+private:
+    int maxSeats;
+    int numZones;
+    int seatsPerZone;
+
+public:
+    EventLocation(int maxSeats, int numZones, int seatsPerZone) {
+        this->maxSeats = maxSeats;
+        this->numZones = numZones;
+        this->seatsPerZone = seatsPerZone;
+    }
+
+    int GetMaxSeats() const {
+        return maxSeats;
+    }
+
+    int GetNumZones() const {
+        return numZones;
+    }
+
+    int GetSeatsPerZone() const {
+        return seatsPerZone;
+    }
+};
+
+class Event {
+private:
+    std::string name;
+    std::string date;
+    std::string time;
+
+public:
+    Event(const std::string& name, const std::string& date, const std::string& time) {
+        this->name = name;
+        this->date = date;
+        this->time = time;
+    }
+
+    const std::string& GetName() const {
+        return name;
+    }
+
+    const std::string& GetDate() const {
+        return date;
+    }
+
+    const std::string& GetTime() const {
+        return time;
+    }
+};
+
