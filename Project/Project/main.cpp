ca1075b7ebe34b0f67c6a67def9ec3ff94b3e2cd
Tudornitu1 <tudornitu@yahoo.com>
2023-11-17 02:13:57 +0200

    main

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <cstring>
 
 class EventLocation {
 private:
@@ -56,6 +57,63 @@ public:
     }
 };
 
+
+class Ticket {
+private:
+    int eventID;
+    std::string type;
+    int seatNumber;
+    std::string uniqueID;
+
+public:
+    static int ticketCount;
+    static int uniqueIDCounter;
+
+    Ticket(int eventID, const std::string& type, int seatNumber) {
+        this->eventID = eventID;
+        this->type = type;
+        this->seatNumber = seatNumber;
+        GenerateUniqueID();
+        ticketCount++;
+    }
+
+    int GetEventID() const {
+        return eventID;
+    }
+
+    const std::string& GetType() const {
+        return type;
+    }
+
+    int GetSeatNumber() const {
+        return seatNumber;
+    }
+
+    const std::string& GetUniqueID() const {
+        return uniqueID;
+    }
+
+    static int GetTicketCount() {
+        return ticketCount;
+    }
+
+    void DisplayTicketInfo() const {
+        std::cout << "Event ID: " << eventID << "\n";
+        std::cout << "Ticket Type: " << type << "\n";
+        std::cout << "Seat Number: " << seatNumber << "\n";
+        std::cout << "Unique ID: " << uniqueID << "\n";
+    }
+
+private:
+    void GenerateUniqueID() {
+        uniqueID = std::to_string(uniqueIDCounter++);
+    }
+};
+
+int Ticket::ticketCount = 0;
+int Ticket::uniqueIDCounter = 1;
+
+
 int main() {
     // Read event location characteristics
     int maxSeats, numZones, seatsPerZone;
@@ -69,12 +127,49 @@ int main() {
     EventLocation eventLocation(maxSeats, numZones, seatsPerZone);
 
     // Read event characteristics
+    std::cin.ignore(); // Consume the newline character left in the buffer
     std::string eventName, eventDate, eventTime;
     std::cout << "Enter event name: ";
-    std::cin.ignore();
     std::getline(std::cin, eventName);
     std::cout << "Enter event date: ";
     std::cin >> eventDate;
     std::cout << "Enter event time: ";
     std::cin >> eventTime;
+
+    Event event(eventName, eventDate, eventTime);
+
+    int eventID = 1;
+    int numTickets;
+    std::cout << "Enter the number of tickets to generate: ";
+    std::cin >> numTickets;
+
+    std::vector<Ticket> tickets;
+
+    for (int i = 0; i < numTickets; i++) {
+        std::string ticketType;
+        int seatNumber;
+
+        std::cout << "Enter ticket type (VIP or Standard): ";
+        std::cin >> ticketType;
+
+        std::cout << "Enter seat number: ";
+        std::cin >> seatNumber;
+
+        if (seatNumber >= 1 && seatNumber <= eventLocation.GetMaxSeats()) {
+            Ticket ticket(eventID, ticketType, seatNumber);
+            tickets.push_back(ticket);
+            std::cout << "Ticket generated with unique ID: " << ticket.GetUniqueID() << "\n";
+        }
+        else {
+            std::cout << "Seat number is invalid. Please try again.\n";
+        }
+    }
+
+    // Display ticket information
+    for (const Ticket& ticket : tickets) {
+        ticket.DisplayTicketInfo();
+        std::cout << "--------------------------------------\n";
+    }
+
+    return 0;
 }
